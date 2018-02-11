7115dc36130dcfac2cc984687239559e86d573ee
Ben Ripkens <bripkens@gmail.com>
2018-02-11 14:05:14 +0100

    Remove too fragile facial recognition

--- facial-recognition/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb', 'prettier'],
-  rules: {
-    'no-use-before-define': 0,
-    'import/prefer-default-export': 0,
-    'global-require': 0,
-    'no-param-reassign': 0
-  }
-};
--- facial-recognition/README.md
+++
@@ -1,9 +0,0 @@
-# facial recognition
-
-## Installation
-
-```
-brew install opencv
-export OPENCV4NODEJS_DISABLE_AUTOBUILD=true
-yarn
-```
